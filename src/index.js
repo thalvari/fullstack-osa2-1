847b7c62191c1688f6e04424f814bf14220a4163
thalvari <tuomas.halvari@helsinki.fi>
2019-02-04 00:10:27 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+            {
+                name: 'Reactin perusteet',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Tiedonvälitys propseilla',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'Komponenttien tila',
+                exercises: 14,
+                id: 3
+            }
+        ]
+    }
+    return <Course course={course}/>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Course = ({course}) => (
+    <div>
+        <Header course={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
+    </div>
+)
+
+const Header = ({course}) => <h1>{course}</h1>
+
+const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part}/>)
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({parts}) => {
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
+    return <p>yhteensä {total} tehtävää</p>
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
